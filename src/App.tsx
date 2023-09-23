28661a838d3221a20fb166e4270be34a6180a5c1
Alekseenko Nikita <trichicode@gmail.com>
2023-09-23 16:02:49 +0300

    Revert "fonts fixes"
    This reverts commit b1a0de7ecd74d08c7ab63218b74d5def0d6cde82.

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
 import {useEffect, useState} from 'react';
 import './App.scss';
 
-import './fonts/e-Ukraine-Regular.otf';
-
 import VerticalSwiper from './components/home/VerticalSwiper';
 import Settings from "./components/settings/Settings";
 import { BookContext } from './context/bookContext';
+import Timeline from "./components/book/Timeline";
 
 const App = () => {
   const [isFocus, setIsFocus] = useState(false);
   const [isPrevAvailable, setIsPrevAvailable] = useState(false);
   const [isNextAvailable, setIsNextAvailable] = useState(true);
   const [currentPage, setCurrentPage] = useState(0);
-
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-      setIsMobile( window.innerWidth <= 1300 );
-  }, []);
 
   return (
     <BookContext.Provider
@@ -32,18 +25,8 @@ const App = () => {
         setIsNextAvailable,
       }}>
           <div className="App">
-              {
-                  !isMobile ? (
-                      <>
-                          <VerticalSwiper />
-                          <Settings />
-                      </>
-                  ) : (
-                      <div className="container">
-                          <h1 style={{fontSize: '2em', width: '80%'}}> Sorry, unavailable for your device ( </h1>
-                      </div>
-                  )
-              }
+            <VerticalSwiper />
+            <Settings />
           </div>
     </BookContext.Provider>
   );
--- src/components/book/Timeline.tsx
+++ src/components/book/Timeline.tsx
@@ -4,12 +4,18 @@ import './Timeline.scss';
 import { TimelineSlider } from "../slider/CustomSlider";
 import {BookContext} from "../../context/bookContext";
 import {BOOK_DATA} from "../../constants/bookData";
+import {useSwiper} from "swiper/react";
 
 const Timeline = () => {
     const {
         currentPage,
         setCurrentPage
     } = useContext(BookContext);
+    const swiper = useSwiper();
+
+    useEffect(() => {
+        console.log(swiper);
+    }, [swiper])
 
     const currentYear = new Date().getFullYear();
 
